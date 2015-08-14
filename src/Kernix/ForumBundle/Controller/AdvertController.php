001639555dc45ab50be76924b2ffa71b2896a293
MatteoB92 <matthieu.boyer92@gmail.com>
2015-08-14 17:31:43 +0200

    Antispam
    Mise en place d'un nouveau service : antispam

--- src/Kernix/ForumBundle/Controller/AdvertController.php
+++ src/Kernix/ForumBundle/Controller/AdvertController.php
@@ -100,4 +100,18 @@ class AdvertController extends Controller
     );
   }
 
+  public function addAction(Request $request)
+  {
+    // On récupère le service
+    $antispam = $this->container->get('oc_platform.antispam');
+
+    // Je pars du principe que $text contient le texte d'un message quelconque
+    $text = '...';
+    if ($antispam->isSpam($text)) {
+      throw new \Exception('Votre message a été détecté comme spam !');
+    }
+    
+    // Ici le message n'est pas un spam
+  }
+
 }
